f674d1c6a836384086f9f52315d4882c4ad8b5b8
diegogalli <84423207+diegogalli@users.noreply.github.com>
2021-08-11 14:35:25 -0300

    Uri 1019 Conversao de tempo

---
+++ ConversaoDeTempo/ConversaoDeTempo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace ConversaoDeTempo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int N, Horas, Resto, minutos, segundos;
+
+            N = int.Parse(Console.ReadLine());
+            Horas = N / 3600;
+            Resto = N % 3600; // % mod = recebe o resto da divisão
+            minutos = Resto / 60;
+            segundos = Resto % 60;
+
+            Console.WriteLine(Horas + ":" + minutos + ":" + segundos);
+
+          
+        }
+    }
+}
